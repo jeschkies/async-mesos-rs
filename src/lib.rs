436416567578ffa3e572a7d6cc1e5962ccf904f2
Karsten Jeschkies <jeskar@web.de>
2018-01-12 11:43:30 +0100

    Connect to http://httpbin.org/ip in test.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+
 #[cfg(test)]
 mod tests {
+
+    use futures::Future;
+	use hyper::Client;
+    use hyper::Uri;
+	use tokio_core::reactor::Core;
+
     #[test]
     fn it_works() {
+		let mut core = Core::new().unwrap();
+        let handle = core.handle();
+        let client = Client::new(&handle);
+
+		let uri = "http://httpbin.org/ip".parse::<Uri>().unwrap();
+		let work = client.get(uri).map(|res| {
+			println!("Response: {}", res.status());
+		}).map(|_| {
+            println!("\nDone.");
+        });
+
+        core.run(work).unwrap();
+
         assert_eq!(2 + 2, 4);
     }
 }
