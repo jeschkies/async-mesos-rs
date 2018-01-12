fee3fac0a57ceb52e4d5be16beffcbabdb27c646
Karsten Jeschkies <jeskar@web.de>
2018-01-12 16:50:36 +0100

    Create RecordIOConnection with simple Stream implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,34 @@ extern crate futures;
 extern crate hyper;
 extern crate tokio_core;
 
+mod mesos {
+
+    use hyper;
+    use futures::{Poll, Stream};
+
+    pub struct RecordIoConnection {
+        pub body: hyper::Body,
+    }
+
+    impl Stream for RecordIoConnection {
+        type Item=hyper::Chunk;
+        type Error=hyper::error::Error;
+
+        fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+            self.body.poll()
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
-    use futures::Future;
+    use std::io::{self, Write};
+
+    use futures::{Future, Stream};
 	use hyper::Client;
     use hyper::Uri;
+    use mesos;
 	use tokio_core::reactor::Core;
 
     #[test]
@@ -19,11 +41,15 @@ mod tests {
 		let uri = "http://httpbin.org/ip".parse::<Uri>().unwrap();
 		let work = client.get(uri).map(|res| {
 			println!("Response: {}", res.status());
-		}).map(|_| {
-            println!("\nDone.");
+            mesos::RecordIoConnection { body: res.body() }
+		});
+
+        let s: mesos::RecordIoConnection = core.run(work).unwrap();
+        let w = s.for_each(|chunk| {
+            io::stdout().write_all(&chunk).map_err(From::from)
         });
 
-        core.run(work).unwrap();
+        core.run(w).unwrap();
 
         assert_eq!(2 + 2, 4);
     }
