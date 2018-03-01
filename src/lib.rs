da1d2c16335353f09edf0137640415b2c7d7e290
Karsten Jeschkies <jeskar@web.de>
2018-03-01 11:02:21 +0100

    Document getting started in module.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
+//! Asynchronous Mesos Client
+//!
+//! This crate provides an asynchronous client for the Mesos
+//! [Scheduler HTTP API](http://mesos.apache.org/documentation/latest/scheduler-http-api).
+//!
+//! ## Installation
+//!
+//! Simply add this to your `Cargo.toml`
+//!
+//! ```toml
+//! [dependencies]
+//! async_mesos = 0.1
+//! ```
+//!
+//! ## Getting Started
+//!
+//! ```rust
+//! # extern crate async_mesos;
+//! # extern crate hyper;
+//! # extern crate tokio_core;
+//! # fn main() {
+//! use async_mesos::mesos;
+//! use async_mesos::client::Client;
+//! use hyper::Uri;
+//! use tokio_core::reactor::Core;
+//!
+//! // Create a Tokio core handle.
+//! let mut core = Core::new().expect("Could not create core.");
+//! let handle = core.handle();
+//!
+//! // Create the Mesos framework info to register a new framework.
+//! let mut framework_info = mesos::FrameworkInfo::new();
+//! framework_info.set_user(String::from("donnie"));
+//! framework_info.set_name(String::from("Example FOO Framework"));
+//!
+//! // Connect to Mesos scheduler API.
+//! let uri = "http://localhost:5050/api/v1/scheduler"
+//!     .parse::<Uri>()
+//!     .expect("Could not parse Uri.");
+//! let client = Client::connect(&handle, uri, framework_info);
+//! # }
+//! ```
+
 extern crate bytes;
 #[macro_use]
 extern crate failure;
