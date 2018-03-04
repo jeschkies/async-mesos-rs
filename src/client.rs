92e28d35814d1b6661187799fb016172358c3737
Karsten Jeschkies <jeskar@web.de>
2018-03-04 18:55:46 +0100

    Document client creation.

--- src/client.rs
+++ src/client.rs
@@ -112,10 +112,47 @@ impl Stream for Events {
 ///
 /// This holds all connection information for a Mesos framework. The framework
 /// id should be persisted to recover after a failover.
+///
+/// # Example
+///
+/// ```no_run
+/// # extern crate async_mesos;
+/// # extern crate futures;
+/// # extern crate hyper;
+/// # extern crate tokio_core;
+/// # fn main() {
+/// use async_mesos::mesos;
+/// use async_mesos::client::{Client, Events};
+/// use hyper::Uri;
+/// use tokio_core::reactor::Core;
+///
+/// // Create a Tokio core handle.
+/// let mut core = Core::new().expect("Could not create core.");
+/// let handle = core.handle();
+///
+/// // Create the Mesos framework info to register a new framework.
+/// let mut framework_info = mesos::FrameworkInfo::new();
+/// framework_info.set_user(String::from("donnie"));
+/// framework_info.set_name(String::from("Example FOO Framework"));
+///
+/// // Connect to Mesos scheduler API.
+/// let uri = "http://localhost:5050/api/v1/scheduler"
+///     .parse::<Uri>()
+///     .expect("Could not parse Uri.");
+/// let future_client = Client::connect(&handle, uri, framework_info);
+/// let (client: Client, events: Events) = core.run(future_client).unwrap();
+/// # }
+/// ```
 #[derive(Clone, Debug)]
 pub struct Client {
+
+    /// The adress of the Mesos scheduler, e.g. `http://localhost:5050/api/v1/scheduler`.
     pub uri: hyper::Uri,
+
+    /// Identification of the registered Mesos framework.
     pub framework_id: String,
+
+    /// Identification of the attached event stream.
     pub stream_id: String,
 }
 
